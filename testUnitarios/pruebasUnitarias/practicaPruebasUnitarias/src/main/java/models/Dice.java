5e5df773173e19e6a554332f4514f2a4a23df7d6
Monica <monibrave1@gmail.com>
2025-07-16 16:48:13 -0300

    creando clase playe y dice

---
+++ testUnitarios/pruebasUnitarias/practicaPruebasUnitarias/src/main/java/models/Dice.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.Random;
+
+//dado = dice
+public class Dice {
+    private int sides;
+
+    public Dice (int sides){
+        this.sides = sides;
+    }
+
+    //devuelve aleatoriamente el valor de la cara del dado
+    public int roll(){
+        return new Random().nextInt(this.sides) + 1;
+    }
+
+}
