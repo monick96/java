74f036c959d0230c0edbc94dede655965e6e8622
Monica <monibrave1@gmail.com>
2025-07-17 17:57:03 -0300

    uso de mockito para simular clase dado y siu resultado

---
+++ testUnitarios/pruebasUnitarias/practicaPruebasUnitarias/src/test/java/models/PlayerTest.java
@@ -0,0 +1,38 @@
+package models;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void lostWhenDiceNumberIsLowOrEqual(){
+        //sin mockito
+        //Dice dice = new Dice(6);
+        //con mockito
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(3);
+
+        Player player = new Player(dice,4);
+
+        assertFalse(player.play());
+    }
+
+    @Test
+    public void winWhenDiceNumberIsHigh(){
+        //sin mockito
+        //Dice dice = new Dice(6);
+        //con mockito
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(3);
+
+        Player player = new Player(dice,2);
+
+        assertTrue(player.play());
+    }
+
+}
